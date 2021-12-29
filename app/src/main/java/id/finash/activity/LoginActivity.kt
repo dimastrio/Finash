b8874d317a26388784ee04e5c5146db5b7d772cd
dimastrio <dimassatrio102@gmail.com>
2021-12-29 14:26:25 +0700

    update

---
+++ app/src/main/java/id/finash/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.finash.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.finash.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
