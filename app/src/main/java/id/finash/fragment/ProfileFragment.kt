5b5a059568daa38c8037373e42ba370a3e3f59a7
dimastrio <dimassatrio102@gmail.com>
2022-01-14 23:38:14 +0700

    avatar

--- app/src/main/java/id/finash/fragment/ProfileFragment.kt
+++ app/src/main/java/id/finash/fragment/ProfileFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import id.finash.R
+import id.finash.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
+
+    private fun setupListener(){
+        binding.imageAvatar.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_avatarFragment)
+        }
     }
 
 }
