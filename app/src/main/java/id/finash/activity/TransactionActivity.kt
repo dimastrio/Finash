b0022501cda2bbbd57383692d7b9263204799f7d
dimastrio <dimassatrio102@gmail.com>
2022-01-15 00:51:47 +0700

    Date Range

--- app/src/main/java/id/finash/activity/TransactionActivity.kt
+++ app/src/main/java/id/finash/activity/TransactionActivity.kt
@@ -2,6 +2,7 @@ package id.finash.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import id.finash.databinding.ActivityTransactionBinding
 import id.finash.fragment.DateFragment
 
@@ -13,11 +14,18 @@ class TransactionActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupListener()
     }
 
     private fun setupListener() {
         binding.imageDate.setOnClickListener{
-
+            DateFragment(object : DateFragment.DateListener{
+                override fun onSucces(dateStart: String, dateEnd: String) {
+                    Log.e("TransactionActivity","$dateStart $dateEnd")
+                }
+            }).apply {
+                show(supportFragmentManager, "dateFragment")
+            }
         }
     }
 }
--- app/src/main/java/id/finash/fragment/DateFragment.kt
+++ app/src/main/java/id/finash/fragment/DateFragment.kt
@@ -5,56 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import id.finash.R
+import id.finash.databinding.FragmentDateBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class DateFragment(var listener: DateListener) : BottomSheetDialogFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDateBinding
+    private var clickDateStart: Boolean = false
+    private var dateTemp: String = ""
+    private var dateStart: String = ""
+    private var dateEnd: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date, container, false)
+        binding = FragmentDateBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setView("Date Start","Select")
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.calendarView.setOnDateChangeListener { _, year, month, day ->
+            dateTemp = "$day/${month + 1}/$year"
+        }
+        binding.textApply.setOnClickListener {
+            when ( clickDateStart ){
+                false -> {
+                    clickDateStart = true
+                    dateStart = dateTemp
+                    binding.calendarView.date = Date().time
+                    setView("Date End","Apply")
+                }
+                true -> {
+                    dateEnd = dateTemp
+                    listener.onSucces(dateStart, dateEnd)
+                    this.dismiss()
                 }
             }
+        }
     }
+
+    private fun setView(title: String, apply: String){
+        binding.textTitle.text = title
+        binding.textApply.text = apply
+    }
+
+    interface DateListener {
+        fun onSucces(dateStart: String, dateEnd: String)
+    }
+
+
 }
